79075cb5a0de2d75265b7750d2423df3b76bdc2a
Данила <danila-dvornikov12@mail.ru>
2025-08-14 12:15:54 +0300

    fix

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Comment, Group, Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',)
@@ -10,6 +11,7 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+@admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'slug', 'description')
     list_display_links = ('title',)
@@ -17,13 +19,9 @@ class GroupAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+@admin.register(Comment) 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'author', 'post', 'created')
     list_editable = ('text',)
     list_display_links = ('pk',)
     empty_value_display = '-пусто-'
-
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Group, GroupAdmin)
-admin.site.register(Comment, CommentAdmin)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -8,27 +8,23 @@ class Group(models.Model):
     title = models.CharField(
         max_length=200,
         verbose_name='Название группы',
-        help_text='Введите название тематической группы',
     )
     slug = models.SlugField(
         max_length=200,
         unique=True,
         verbose_name='Номер группы',
-        help_text='Укажите порядковый номер группы',
     )
     description = models.TextField(
         max_length=200,
         verbose_name='Описание группы',
-        help_text='Добавьте текст описания группы',
     )
 
     class Meta:
-        ordering = ('-title',)
         verbose_name = 'Группа статей'
         verbose_name_plural = 'Группы статей'
 
     def __str__(self) -> str:
-        return self.title[:10]
+        return self.title
 
 
 class Post(models.Model):
@@ -67,12 +63,12 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = ('pub_date',)
+        ordering = ('-pub_date',)
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
 
     def __str__(self) -> str:
-        return self.title[:10]
+        return self.text[:10]
 
 
 class Comment(models.Model):
@@ -103,9 +99,8 @@ class Comment(models.Model):
     )
 
     class Meta:
-        ordering = ('-created',)
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
 
     def __str__(self) -> str:
-        return self.title[:10]
+        return self.text[:15]
